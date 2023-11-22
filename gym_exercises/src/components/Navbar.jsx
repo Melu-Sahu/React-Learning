d53d7af3077ef27f82a68b3566a52b1c22e812b4
Melu-Sahu <melusahu0143.s@gmail.com>
2023-11-22 13:26:17 +0530

    some more changes

--- gym_exercises/src/components/Navbar.jsx
+++ gym_exercises/src/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
       <Link to={"/"}>
         <img src={Logo} alt='logo' style={{ width: '48px', height: '48px', margin: '0 20px' }} />
       </Link>
+      
 
       <Stack
         direction="row"
